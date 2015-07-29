ea867771a9832e4cb6027a5381c982a1bb1e7583
Ruben De Smet <ruben.de.smet@telenet.be>
2015-07-29 15:38:04 +0200

    Advance _data_insert_position, so we don't overwrite stuff

--- src/tdms_segment.cpp
+++ src/tdms_segment.cpp
@@ -317,6 +317,7 @@ void segment_object::_read_values(const unsigned char*& data, endianness e)
         {
             _data_type.read_to(data + (i*_data_type.ctype_length), read_data + (i*_data_type.ctype_length));
         }
+        _tdms_object->_data_insert_position += (_number_values*_data_type.ctype_length);
         data += (_number_values*_data_type.ctype_length);
     }
 }
